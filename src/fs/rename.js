7fdc9419ceec6d4bc0047f5c2d238e2830631916
kseniya shukevich <kseniya.shukevich@gmail.com>
2022-12-22 18:54:56 +0100

    feat: add rename file function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,22 @@
-const rename = async () => {
-    // Write your code here 
+import fs from 'fs';
+
+import { checkExists } from './helpers.js';
+import { errorMessage } from './constants.js';
+
+const urlWrongFile = 'src/fs/files/wrongFilename.txt';
+const urlCorrectFile = 'src/fs/files/properFilename.md';
+
+const rename = async (urlWrongFile, urlCorrectFile) => {
+    const isWrongFileExists = await checkExists(urlWrongFile);
+    const isCorrectFileExists = await checkExists(urlCorrectFile);
+
+    if (!isWrongFileExists || isCorrectFileExists) {
+        throw new Error(errorMessage);
+    }
+
+    fs.rename(urlWrongFile, urlCorrectFile, (err) => {
+        if (err) throw err;
+    });
 };
 
-await rename();
+await rename(urlWrongFile, urlCorrectFile);
