44a8f047fb16451489fbfaa419a3e4c2ef7d1165
kseniya shukevich <kseniya.shukevich@gmail.com>
2022-12-22 19:26:58 +0100

    feat: add read file function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,23 @@
-const read = async () => {
-    // Write your code here 
+import { readFile } from 'fs';
+
+import { checkExists } from './helpers.js';
+import { errorMessage } from './constants.js';
+
+const urlFileToPrint = './files/fileToRead.txt';
+
+const read = async (urlFileToPrint) => {
+    const filePath = new URL(urlFileToPrint, import.meta.url);
+    const isFileExists = await checkExists(filePath);
+
+    if (!isFileExists) {
+        throw new Error(errorMessage);
+    }
+
+    readFile(filePath, { encoding: 'utf8' }, (err, data) => {
+        if (err) throw err;
+
+        console.log(data);
+    });
 };
 
-await read();
+await read(urlFileToPrint);
