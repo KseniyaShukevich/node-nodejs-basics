006954d4696135eb10fa8b3c71932c4ac5355727
kseniya shukevich <kseniya.shukevich@gmail.com>
2022-12-22 20:06:15 +0100

    feat: add parse env function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,19 @@
-const parseEnv = () => {
-    // Write your code here 
+const startString = 'RSS_';
+
+const parseEnv = (startString) => {
+    const arrayOfEnvironments = Object.entries(process.env);
+
+    const rssEnvironments = arrayOfEnvironments.filter((item) => {
+        const [name, _] = item;
+
+        return name.startsWith(startString)
+    });
+
+    rssEnvironments.forEach((item) => {
+        const [name, value] = item;
+
+        console.log(`${name}=${value}`);
+    })
 };
 
-parseEnv();
+parseEnv(startString);
