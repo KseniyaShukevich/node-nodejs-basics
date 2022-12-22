aae95bff366a47f42ea428ae7be53a1da425d328
kseniya shukevich <kseniya.shukevich@gmail.com>
2022-12-22 19:02:20 +0100

    feat: add delete file function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
-const remove = async () => {
-    // Write your code here 
+import { rm } from 'fs';
+
+import { checkExists } from './helpers.js';
+import { errorMessage } from './constants.js';
+
+const urlFileToDelete = 'src/fs/files/fileToRemove.txt';
+
+const remove = async (urlFileToDelete) => {
+    const isFileToDeleteExists = await checkExists(urlFileToDelete);
+
+    if (!isFileToDeleteExists) {
+        throw new Error(errorMessage);
+    }
+
+    rm(urlFileToDelete, (err) => {
+        if (err) throw err;
+    });
 };
 
-await remove();
+await remove(urlFileToDelete);
