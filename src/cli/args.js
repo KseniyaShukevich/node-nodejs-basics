64c95e33828b48d3494bde40cf567efe9ae5ae83
kseniya shukevich <kseniya.shukevich@gmail.com>
2022-12-22 20:15:17 +0100

    feat: add parse argumnets from cli function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
+const indexStartArguments = 2;
+
 const parseArgs = () => {
-    // Write your code here 
+    const argumetns = process.argv.slice(indexStartArguments);
+
+    for (let i = 0; i < argumetns.length; i += 2) {
+        console.log(`${argumetns[i]} is ${argumetns[i + 1]}`);
+    }
 };
 
 parseArgs();
