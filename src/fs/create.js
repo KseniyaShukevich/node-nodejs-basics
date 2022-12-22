6e9f78860053faf972b2bf76f32ed4cf11bb4921
kseniya shukevich <kseniya.shukevich@gmail.com>
2022-12-22 16:52:07 +0100

    feat: add create file function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
-const create = async () => {
-    // Write your code here 
+import { writeFile } from 'fs';
+
+import { checkFileExists } from './helpers.js';
+import { errorMessage } from './constants.js';
+
+const url = 'src/fs/files/fresh.txt';
+const text = 'I am fresh and young';
+
+const create = async (url, text) => {
+    const isFileExist = await checkFileExists(url)
+
+    if (isFileExist) {
+        throw Error(errorMessage);
+    }
+
+    writeFile(url, text, (err) => {
+        if (err) throw err;
+    })
 };
 
-await create();
+await create(url, text);
