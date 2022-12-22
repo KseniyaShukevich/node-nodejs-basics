9fa7651b0873cbeae27db16d1cb02bc06c98d79e
kseniya shukevich <kseniya.shukevich@gmail.com>
2022-12-22 19:10:43 +0100

    feat: add list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
-const list = async () => {
-    // Write your code here 
+import { readdir } from 'fs';
+
+import { checkExists } from './helpers.js';
+import { errorMessage } from './constants.js';
+
+const urlFolderToPrint = 'src/fs/files';
+
+const list = async (urlFolderToPrint) => {
+    const isFolderExists = checkExists(urlFolderToPrint);
+
+    if (!isFolderExists) {
+        throw new Error(errorMessage);
+    }
+
+    readdir(urlFolderToPrint, (err, files) => {
+        if (err) throw err;
+
+        console.log(files);
+      });
 };
 
-await list();
+await list(urlFolderToPrint);
