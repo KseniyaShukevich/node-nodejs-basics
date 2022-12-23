4ff76fa73b790c726ba5189b349f8bbcc47b7102
kseniya shukevich <kseniya.shukevich@gmail.com>
2022-12-24 00:19:41 +0100

    feat: add write stream function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
-const write = async () => {
-    // Write your code here 
+import { createWriteStream } from 'fs';
+
+const urlFileToWrite = './files/fileToWrite.txt';
+
+const write = async (urlFileToWrite) => {
+    const filePathToWrite = new URL(urlFileToWrite, import.meta.url);
+    const fileWriteStream = createWriteStream(filePathToWrite, {
+        flags: 'a',
+      });
+
+    process.stdin.pipe(fileWriteStream);
 };
 
-await write();
+await write(urlFileToWrite);
