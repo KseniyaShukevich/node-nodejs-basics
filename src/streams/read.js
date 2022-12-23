0309322bbfd7bf1217479f126a78e174c5adc050
kseniya shukevich <kseniya.shukevich@gmail.com>
2022-12-23 23:25:17 +0100

    feat: add read stream function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
-const read = async () => {
-    // Write your code here 
+import { createReadStream } from 'fs';
+
+const urlFileToRead = './files/fileToRead.txt';
+
+const read = async (urlFileToRead) => {
+    const filePath = new URL(urlFileToRead, import.meta.url);
+    const fileStream = createReadStream(filePath);
+
+    fileStream.on('readable', () => {
+        const data = fileStream.read();
+
+        if (data) {
+            console.log(`${data}`);
+        }
+    });
 };
 
-await read();
+await read(urlFileToRead);
