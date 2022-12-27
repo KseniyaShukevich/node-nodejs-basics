a747812feb06f653a9c757b36d310190a7b6c711
kseniya shukevich <kseniya.shukevich@gmail.com>
2022-12-27 19:20:12 +0100

    feat: add compress and decompress functions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,16 @@
-const compress = async () => {
-    // Write your code here 
+import { createReadStream, createWriteStream } from 'fs';
+import zlib from 'zlib';
+
+const urlFileToCompress = './files/fileToCompress.txt';
+const urlFileToWrite = './files/archive.gz';
+
+const compress = async (urlFileToCompress, urlFileToWrite) => {
+    const fileToReadPath = new URL(urlFileToCompress, import.meta.url);
+    const fileToWritePath = new URL(urlFileToWrite, import.meta.url);
+    const fileReadStream = createReadStream(fileToReadPath);
+    const fileWriteStream = createWriteStream(fileToWritePath);
+
+    fileReadStream.pipe(zlib.createGzip()).pipe(fileWriteStream);
 };
 
-await compress();
+await compress(urlFileToCompress, urlFileToWrite);
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,16 @@
-const decompress = async () => {
-    // Write your code here 
+import { createReadStream, createWriteStream } from 'fs';
+import zlib from 'zlib';
+
+const urlFileToDecompress = './files/archive.gz';
+const urlFileToWrite = './files/fileToCompress.txt';
+
+const decompress = async (urlFileToDecompress, urlFileToWrite) => {
+    const fileToReadPath = new URL(urlFileToDecompress, import.meta.url);
+    const fileToWritePath = new URL(urlFileToWrite, import.meta.url);
+    const fileReadStream = createReadStream(fileToReadPath);
+    const fileWriteStream = createWriteStream(fileToWritePath);
+
+    fileReadStream.pipe(zlib.createGunzip()).pipe(fileWriteStream);
 };
 
-await decompress();
+await decompress(urlFileToDecompress, urlFileToWrite);
